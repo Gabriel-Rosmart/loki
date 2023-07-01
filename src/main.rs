871aa4544a8bfba50c4f6b415a3642468cd71328
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-07-01 12:38:00 +0200

    Accepting query strign from command line

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use loki::{
 type DocumentIndexMap = HashMap<String, HashMap<String, usize>>;
 
 fn main() {
+    let query: &str = &std::env::args().collect::<Vec<String>>()[1];
+
     let mut path = std::env!("CARGO_MANIFEST_DIR").to_string();
 
     path.push_str("/assets");
@@ -28,31 +30,17 @@ fn main() {
         );
     }
 
-    // println!("{document_map:#?}");
-    //
-    // let search_query: Vec<char> = "rust programming language docs".chars().collect();
-    //
-    // for token in Lexer::new(&search_query) {
-    //     println!("{token:?}");
-    // }
-
     let mut ranks = Vec::<(String, f32)>::new();
 
     for (path, freq_table) in &document_map {
         let mut total_rank = 0f32;
 
-        for token in Lexer::new(&"horse".chars().collect::<Vec<char>>()) {
+        for token in Lexer::new(&query.chars().collect::<Vec<char>>()) {
             total_rank += Indexer::term_frequency(&token, &freq_table)
                 * Indexer::inverse_document_frequency(&token, &document_map);
-
-            // println!(
-            //     "  {token} => {rank}",
-            //     rank = Indexer::inverse_document_frequency(&token, &document_map)
-            // );
         }
 
         ranks.push((path.to_string(), total_rank));
-        // println!("{path} => {total_rank}");
     }
 
     ranks.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).unwrap());
